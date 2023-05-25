b2cf149677e69d2d196adbd56de6c54b0ec713dc
Shashi Kiran <c.shashikiran421@gmail.com>
2023-05-25 19:31:11 +0530

    added streamlit feature

--- backend/core.py
+++ backend/core.py
@@ -1,10 +1,11 @@
 import os
+from typing import Any
+
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.vectorstores import Pinecone
 import pinecone
-
 
 
 pinecone.init(
@@ -13,20 +14,20 @@ pinecone.init(
 )
 
 
-def run_llm(query: str) ->any:
+def run_llm(query: str) -> Any:
     embeddings = OpenAIEmbeddings()
-    doc_search = Pinecone.from_existing_index(
+    docsearch = Pinecone.from_existing_index(
         index_name="langchain-doc-index", embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-
     qa = RetrievalQA.from_chain_type(
         llm=chat,
         chain_type="stuff",
-        retriever=doc_search.as_retriever(),
+        retriever=docsearch.as_retriever(),
         return_source_documents=True,
     )
     return qa({"query": query})
 
+
 if __name__ == "__main__":
-    print(run_llm(query="What is Langchain?"))
+    print(run_llm(query="What is LangChain?"))
